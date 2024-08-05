308fb655183d61fd9f2d595ae407b4998219d36a
Alaguraj1 <alaguraj259@gmail.com>
2024-08-05 10:42:09 +0530

    careers section changes

--- app/careers/page.js
+++ app/careers/page.js
@@ -18,7 +18,7 @@ export default function Careers() {
               </h2>
             </div> */}
             <div class="row clearfix">
-              <div class="col-lg-6 col-md-12 col-sm-12 order-lg-2 content-column">
+              <div class="col-lg-12 col-md-12 col-sm-12 order-lg-2 content-column text-center">
                 <div className="content_block_two">
                   <div className="content-box p_relative d_block">
                     <div className="sec-title mb_5">
@@ -26,13 +26,19 @@ export default function Careers() {
                       <h3>Are You</h3>
                     </div>
 
-                    <div className="text mb_25">
-                      <ul className="about-list-style clearfix mb_25">
+                    <div className="text">
+                      {/* <ul className="about-list-style clearfix mb_25">
                         <li>Ready to color outside the lines?</li>
                         <li>An out of the box thinker?</li>
                         <li>Willing to traverse the extra mile?</li>
                         <li>Ready to explore new horizons?</li>
-                      </ul>
+                      </ul> */}
+                      <div className="text mb_25">
+                        <p>Ready to color outside the lines?</p>
+                        <p>An out of the box thinker?</p>
+                        <p>Willing to traverse the extra mile?</p>
+                        <p>Ready to explore new horizons?</p>
+                      </div>
                       <h5 style={{ fontWeight: "600" }}>
                         If you answer is YES, then KPR Mill is the right place.
                       </h5>
@@ -46,7 +52,7 @@ export default function Careers() {
                   </div>
                 </div>
               </div>
-              <div class="col-lg-6 col-md-12 col-sm-12 order-lg-1 image-column">
+              {/* <div class="col-lg-6 col-md-12 col-sm-12 order-lg-1 image-column">
                 <div class="image-box pl-3">
                   <figure class="image">
                     <img
@@ -55,7 +61,7 @@ export default function Careers() {
                     />
                   </figure>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </section>
@@ -139,20 +145,32 @@ export default function Careers() {
               </span>
               <h2>YARN</h2>
             </div> */}
-            <div class="row clearfix">
-              <div class="col-lg-12 col-md-12 col-sm-12 content-column text-center">
-                <iframe
-                  src="https://docs.google.com/forms/d/e/1FAIpQLSfm0IkZj3J3rXcI9qp_hT8AAZ3zTnyyCfQibQGr1YTqs9Xs2A/viewform?embedded=true"
-                  width="100%"
-                  height="1500"
-                  frameborder="0"
-                  marginwidth="0"
-                  marginheight="0"
-                >
-                  Loading&#8230;
-                </iframe>
-              </div>
-            </div>
+             <div class="row clearfix">
+               
+
+               <div class="col-lg-6 col-md-12 col-sm-12 order-lg-2 content-column text-center">
+                 <iframe
+                   src="https://docs.google.com/forms/d/e/1FAIpQLSfm0IkZj3J3rXcI9qp_hT8AAZ3zTnyyCfQibQGr1YTqs9Xs2A/viewform?embedded=true"
+                   width="100%"
+                   height="1500"
+                   frameborder="0"
+                   marginwidth="0"
+                   marginheight="0"
+                 >
+                   Loading&#8230;
+                 </iframe>
+               </div>
+               <div class="col-lg-6 col-md-12 col-sm-12 order-lg-1 image-column">
+                 <div class="image-box pl-3">
+                   <figure class="image">
+                     <img
+                       src="/assets/images/kprmill-images/Careers/careers-1.png"
+                       alt=""
+                     />
+                   </figure>
+                 </div>
+               </div>
+             </div>
           </div>
         </section>
       </Layout>
