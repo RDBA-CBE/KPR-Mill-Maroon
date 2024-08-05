31f360996e44466d102be8e47dca20761cc72759
Alaguraj1 <alaguraj259@gmail.com>
2024-08-05 14:39:10 +0530

    changes

--- components/sections/home4/Countries.js
+++ components/sections/home4/Countries.js
@@ -1,12 +1,22 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-export default function Countries()  {
+export default function Countries() {
   return (
-    <section className="countries-style-five p_relative" >
+    <section className="countries-style-five p_relative">
       <div className="pattern-layer">
-        <div className="pattern-1" style={{ backgroundImage: 'url(/assets/images/kprmill/home/shape-1.png)' }}></div>
-        <div className="pattern-2" style={{ backgroundImage: 'url(/assets/images/kprmill/home/shape-2.png)' }}></div>
+        <div
+          className="pattern-1"
+          style={{
+            backgroundImage: "url(/assets/images/kprmill/home/shape-1.png)",
+          }}
+        ></div>
+        <div
+          className="pattern-2"
+          style={{
+            backgroundImage: "url(/assets/images/kprmill/home/shape-2.png)",
+          }}
+        ></div>
       </div>
       <div className="auto-container">
         <div className="sec-title mb_60 text-center light">
@@ -15,10 +25,19 @@ export default function Countries()  {
         </div>
         <div className="row clearfix">
           <div className="col-lg-3 col-md-6 col-sm-12 countries-block">
-            <div className="countries-block-five wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+            <div
+              className="countries-block-five wow fadeInUp animated"
+              data-wow-delay="00ms"
+              data-wow-duration="1500ms"
+            >
               <div className="inner-box">
                 <div className="image-box">
-                  <figure className="image"><img src="/assets/images/kprmill-images/Home-Page/whats-new.png" alt="" /></figure>
+                  <figure className="image">
+                    <img
+                      src="/assets/images/kprmill-images/Home-Page/whats-new.png"
+                      alt=""
+                    />
+                  </figure>
                 </div>
                 {/* <div className="lower-content">
                   <h5><Link href="index-4">China <span>Visa</span></Link></h5>
@@ -28,10 +47,21 @@ export default function Countries()  {
             </div>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12 countries-block">
-            <div className="countries-block-five wow fadeInUp animated" data-wow-delay="200ms" data-wow-duration="1500ms">
+            <div
+              className="countries-block-five wow fadeInUp animated"
+              data-wow-delay="200ms"
+              data-wow-duration="1500ms"
+            >
               <div className="inner-box">
                 <div className="image-box">
-                  <figure className="image"><img src="/assets/images/kprmill-images/Home-Page/about-us.png" alt="" /></figure>
+                  <Link href="/about-us" style={{cursor: "pointer"}}>
+                    <figure className="image">
+                      <img
+                        src="/assets/images/kprmill-images/Home-Page/about-us.png"
+                        alt=""
+                      />
+                    </figure>
+                  </Link>{" "}
                 </div>
                 {/* <div className="lower-content">
                   <h5><Link href="index-4">United Kingdom <span>Visa</span></Link></h5>
@@ -41,10 +71,21 @@ export default function Countries()  {
             </div>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12 countries-block">
-            <div className="countries-block-five wow fadeInUp animated" data-wow-delay="400ms" data-wow-duration="1500ms">
+            <div
+              className="countries-block-five wow fadeInUp animated"
+              data-wow-delay="400ms"
+              data-wow-duration="1500ms"
+            >
               <div className="inner-box">
                 <div className="image-box">
-                  <figure className="image"><img src="/assets/images/kprmill-images/Home-Page/awards.png" alt="" /></figure>
+                  <Link href="/awards" style={{cursor: "pointer"}}>
+                    <figure className="image">
+                      <img
+                        src="/assets/images/kprmill-images/Home-Page/awards.png"
+                        alt=""
+                      />
+                    </figure>
+                  </Link>
                 </div>
                 {/* <div className="lower-content">
                   <h5><Link href="index-4">Brazil <span>Visa</span></Link></h5>
@@ -54,10 +95,21 @@ export default function Countries()  {
             </div>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12 countries-block">
-            <div className="countries-block-five wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms">
+            <div
+              className="countries-block-five wow fadeInUp animated"
+              data-wow-delay="600ms"
+              data-wow-duration="1500ms"
+            >
               <div className="inner-box">
                 <div className="image-box">
-                  <figure className="image"><img src="/assets/images/kprmill-images/Home-Page/cert.png" alt="" /></figure>
+                  <Link href="/certifications" style={{cursor: "pointer"}}>
+                  <figure className="image">
+                    <img
+                      src="/assets/images/kprmill-images/Home-Page/cert.png"
+                      alt=""
+                    />
+                  </figure>
+                  </Link>
                 </div>
                 {/* <div className="lower-content">
                   <h5><Link href="index-4">Australia <span>Visa</span></Link></h5>
@@ -70,5 +122,4 @@ export default function Countries()  {
       </div>
     </section>
   );
-};
-
+}
