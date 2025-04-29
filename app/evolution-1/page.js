4946c56e565a8f516e2ec0311e51b7b8b36a0c23
nsathiyakala <nsathiyakala6@gmail.com>
2025-04-29 14:54:27 +0530

    evolution

--- app/evolution-1/page.js
+++ app/evolution-1/page.js
@@ -1,13 +1,28 @@
 "use client";
 import Layout from "@/components/layout/Layout";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { FaBuilding, FaUserGraduate, FaStar } from "react-icons/fa";
+import Image from "next/image";
+import { CustomDate } from "@/utils/constant.utils";
 
 import { useEffect } from "react";
 
 import axios from "axios";
 import { useSetState } from "@/utils/states.utils";
+
+const WorkIcon = () => <FaBuilding />;
+const EducationIcon = () => <FaUserGraduate />;
+const StarIcon = () => <FaStar />;
+
 export default function Home() {
   const [state, setState] = useSetState({
     aboutPage: {},
+    evolutionData:[]
+
   });
 
   useEffect(() => {
@@ -22,7 +37,7 @@ export default function Home() {
 
       if (res?.data?.length > 0) {
         setState({
-          aboutPage: res?.data?.[0]?.content?.rendered,
+          aboutPage: res?.data?.[0],
         });
       } else {
         console.error("Page not found");
@@ -32,6 +47,63 @@ export default function Home() {
     }
   };
 
+  // console.log("data" ,state.aboutPage);
+
+  useEffect(() => {
+
+    if (state.aboutPage?.content?.rendered) {
+      const evolutionData = parseImageData(state.aboutPage.content.rendered);
+     
+      
+      setState({
+        evolutionData:evolutionData
+      });
+   
+    }
+  }, [state.aboutPage]);
+
+  console.log("evolutionData",state.evolutionData);
+
+  const parseImageData = (htmlContent) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlContent, "text/html");
+    const EvolutionCon = doc.querySelector('div[data-id="c50233f"]');
+
+    console.log("EvolutionCon",EvolutionCon);
+    
+
+    if (!EvolutionCon) return [];
+
+    const EvolutionChil = EvolutionCon.querySelectorAll(".evolution-Chil");
+    const cleanText = (text) => text?.replace(/[\n\t]+/g, ' ').replace(/\s+/g, ' ').trim() || '';
+
+    console.log("EvolutionChil",EvolutionChil);
+    
+
+
+    return Array.from(EvolutionChil).map((data) => {
+
+      console.log("EvolutionChil", data);
+      
+      const img = data.querySelector("img");
+      const year = cleanText(data.querySelector(".evolution-year")?.textContent);
+      const title = cleanText(data.querySelector(".evolution-title")?.textContent);
+      const paragraph = cleanText(data.querySelector(".evolution-desc")?.textContent);
+
+      console.log("title",title);
+      
+      
+      return {
+        imgSrc: img?.getAttribute("src") || null,
+        title: title || "",
+        year:year || "",
+        paragraph: paragraph || "",
+        imgAlt: img?.getAttribute("alt") || "",
+      };
+    });
+  };
+  
+
   const BannerImage =
     "/assets/images/kprmill-images/About/evolution-banner-img.jpg";
 
@@ -40,11 +112,56 @@ export default function Home() {
       <Layout
         headerStyle={2}
         footerStyle={2}
-        breadcrumbTitle="The Board"
+        breadcrumbTitle="Evolution"
         imageUrl={BannerImage}
       >
-        <div dangerouslySetInnerHTML={{ __html: state.aboutPage }} />
+        {/* <div dangerouslySetInnerHTML={{ __html: state.aboutPage.content.rendered }} /> */}
+
+          <section className="dream-style-three p_relative">
+                  <div className="auto-container">
+                    <div className="sec-title  mb_30 centred">
+                      <span className="sub-title" style={{ color: "black" }}>
+                      Our significant breakthroughs
+                      </span>
+                      <h2 style={{ fontSize: "18px", lineHeight: "30px" }}>
+                        The fore vision and enterprising efforts of KPR leaders has
+                        resulted in one of the India’s largest vertically integrated
+                        Apparel manufacturing Companies. Right from day one, KPR is
+                        consistently driven by values, ethics and principles.
+                      </h2>
+                    </div>
+
+                    {state?.evolutionData?.map((data,index)=>(
+                       <VerticalTimeline>
+                           <VerticalTimelineElement
+                                          key={index}
+                                          visible={true}
+                                          className="vertical-timeline-element--work"
+                                          date={
+                                            <CustomDate
+                                              date={data?.year}
+                                              firstLine={data?.title}
+                                              secondLine={data?.paragraph}
+                                            />
+                                          }
+                                          iconStyle={{ background: "#daad19", color: "#fff" }}
+                                          icon={<WorkIcon />}
+                                        >
+                                          <img
+                                            src={data?.imgSrc}
+                                            alt={data?.imgAlt}
+                                            width={400}
+                                            height={300}
+                                          />
+                            </VerticalTimelineElement>
+                        </VerticalTimeline>
+                        
+                    ))}
+                   
+                  </div>
+                </section>
       </Layout>
+      
     </>
   );
 }
